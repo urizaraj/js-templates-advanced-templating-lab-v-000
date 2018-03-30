56219b8e7dea937d2c07fa6a92ad19a064bf291d
Alexander Urizar <urizaraj@gmail.com>
2018-03-30 17:39:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const recipeFormTemplate = document.getElementById('recipe-form-template')
 
 function init() {
   const template = Handlebars.compile(recipeFormTemplate)
+  const html = template()
 }
 document.addEventListener("DOMContentLoaded", function(event) {
   init()
