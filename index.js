83049ca3af6408154c4cb62ad8bda11fb31ca0be
Alexander Urizar <urizaraj@gmail.com>
2018-03-30 19:11:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function init() {
 
 function createRecipe() {
   let inodes = document.getElementsByName('ingredients')
-  let ingredients = inodes.map(node => node.value)
+  let ingredients = (inodes ? inodes.map(node => node.value) : [])
   recipe = {
     name: document.getElementById('name').value,
     description: document.getElementById('description').value,
